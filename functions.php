a0eba4374ea0dd8bb2c30af22bd02fb45e62198c
Nikeda <4104cafeaulait@gmail.com>
2020-01-11 19:21:42 +0900

    アイコン関連

--- functions.php
+++ functions.php
@@ -3,6 +3,14 @@ function set_up(){
     add_theme_support("responsive-embeds");
 
     add_theme_support("wp-block-styles");
+    add_theme_support("title-tag");
+
+    //html5の有効化
+    add_theme_support("html5",array(
+        "style",
+        "script",
+        "comment-form"
+    ));
 }
 add_action( "after_setup_theme","set_up");
 
@@ -13,5 +21,8 @@ function script(){
         array(),
         filemtime(get_theme_file_path("style.css"))
     );
+    wp_enqueue_script("jquery",'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+    wp_enqueue_script( "my-js", get_stylesheet_directory_uri()."/js/script.js");
+    wp_enqueue_style("dashicons");
 };
 add_action("wp_enqueue_scripts","script");
--- single.php
+++ single.php
@@ -2,12 +2,20 @@
 <?php if(have_posts()):
         while(have_posts()):
             the_post(); ?>
+            <div class="cat-cont">
+                <?php the_category(); ?>
+            </div>
             <?php the_title('<h1>',"</h1>"); ?>
             <div class="container">
+                <time datetime="<?php echo esc_attr(get_the_date(DATE_W3C)); ?>">
+                    <?php echo esc_html(get_the_date("Y年m月d日")); ?>
+                </time>
                 <?php the_content();?>
+                <?php the_post_navigation(); ?>
             </div>
         <?php endwhile;
     endif;
 ?>
 </body>
 </html>
+
