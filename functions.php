90721407feda8dd658dd1f99c74750217962165c
Nikeda <4104cafeaulait@gmail.com>
2020-01-11 12:49:57 +0900

    バージョン日付更新に変更第三引数変更

--- functions.php
+++ functions.php
@@ -1,10 +1,17 @@
 <?php 
 function set_up(){
     add_theme_support("responsive-embeds");
+
+    add_theme_support("wp-block-styles");
 }
 add_action( "after_setup_theme","set_up");
 
 function script(){
-    wp_enqueue_style( "mytheme-style", get_stylesheet_uri());
+    wp_enqueue_style(
+        "mytheme-style",
+        get_stylesheet_uri(),
+        array(),
+        filemtime(get_theme_file_path("style.css"))
+    );
 };
 add_action("wp_enqueue_scripts","script");
