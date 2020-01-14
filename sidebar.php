a9f19b7630efe3be718441a85c7c8d783c4ef131
Nikeda <4104cafeaulait@gmail.com>
2020-01-14 22:29:47 +0900

    7-2

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,11 @@
+<section class="myprofile">
+    <h2 class="widgettitle">PROFILE</h2>
+    <figure>
+        <img src="<?php echo esc_attr(get_stylesheet_directory_uri().'/images/profile.jpg'); ?>" alt="profile">
+    </figure>
+    <strong><?php the_author_meta("display_name"); ?></strong>
+    <p><?php the_author_meta("description"); ?></p>
+</section>
 <?php if(is_active_sidebar("sidebar-a")): ?>
     <?php dynamic_sidebar("sidebar-a") ?>
 <?php endif; ?>
